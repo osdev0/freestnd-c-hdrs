10c37771247545fe5afb4edc27edc39be47d9e56
Mintsuki <mintsuki@protonmail.com>
2025-08-29 04:21:46 +0200

    Update gcc to 14.3.0 and binutils to 2.45

--- make_toolchain.sh
+++ make_toolchain.sh
@@ -15,11 +15,11 @@ if [ -z "$TARGET" ]; then
 fi
 
 if [ -z "$BINUTILSVERSION" ]; then
-    BINUTILSVERSION=2.44
+    BINUTILSVERSION=2.45
 fi
 
 if [ -z "$GCCVERSION" ]; then
-    GCCVERSION=14.2.0
+    GCCVERSION=14.3.0
 fi
 
 if command -v gmake; then
